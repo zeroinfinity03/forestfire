f402eb00cc664306fa7f803e4b987efeaa54e9bd
zeroinfinity03 <zeroinfinity03@gmail.com>
2023-10-04 19:53:10 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+
+app = Flask(__name__)
+
+#  import ridge regressor model and standard scaler pickle file
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scalar=pickle.load(open('models/scaler.pkl','rb'))
+
+
+#  route for homepage
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# route for predict data
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+# note the order of input is important
+
+        new_data_scaled=standard_scalar.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',result=result[0])
+        
+    else:
+        return render_template('home.html')
+
+
+
+
+
+if __name__==("__main__"):
+    app.run(host="0.0.0.0")
+
+# bydefault  port=5000 in flask
+# to change the port use:-
+# if __name__==("__main__"):
+    # app.run(host="0.0.0.0",port=2000)
+
+
+
